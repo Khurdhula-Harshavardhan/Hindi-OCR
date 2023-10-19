6f235d35a14c8805b16ecd2b261e2d06e891f4e3
Harsha <khurdhulaharshavardhan@gmail.com>
2023-10-19 16:22:26 -0400

    initializes the module.

---
+++ hindi_ocr.py
@@ -0,0 +1,10 @@
+"""
+Module Description: This module serves an API, for using CNN and a RNN.
+"""
+
+class CNN():
+    """
+    The CNN class is the API for the previously trained model. Which uses CNN.joblib that was trained previously by @Harsha Vardhan Khurdula.
+    """
+    def __init__(self) -> None:
+        pass
