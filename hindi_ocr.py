473695e84ff2bb3efd3e4b21e103d7ea5b510c11
Harsha <khurdhulaharshavardhan@gmail.com>
2023-10-19 20:00:04 -0400

    RNN now is able to take encoded data input.

--- hindi_ocr.py
+++ hindi_ocr.py
@@ -40,6 +40,22 @@ class Image_Processor():
             return {"Result": "Failed",
                     "ERROR": str(e),
                     "ERR at": "hindi_ocr.Image_Processor.read_image"}
+        
+    def read_grayscale_image(self, image_data: bytes) -> float:
+        """
+        Image_Processor.read_image(): takes in the encoded image data and then converts into an numpy array for further processing of the image.
+        """
+        try:
+            self.raw_image_data = image_data
+            self.image_data = base64.b64decode(self.raw_image_data)
+            self.image = image.img_to_array(image.load_img(BytesIO(self.image_data), target_size=(32, 32), color_mode="grayscale"))
+            
+
+            return self.image
+        except Exception as e:
+            return {"Result": "Failed",
+                    "ERROR": str(e),
+                    "ERR at": "hindi_ocr.Image_Processor.read_grayscale_image"}
 
 class CNN(Image_Processor):
     """
@@ -109,7 +125,64 @@ class RNN(Image_Processor):
     """
     The RNN class is the API for the previously trained model. Which uses RNN.joblib that was trained previously by @Harsha Vardhan Khurdula.
     """
-    pass
+    rnn = None
+    base_image_data = None
+    image = None
+
+    def __init__(self) -> None:
+        """
+        The constructor should initialize the model.
+        """
+        try:
+            self.rnn = load("Models/RNN.joblib")
+
+            if self.rnn is None:
+                raise FileNotFoundError("Cannot load pretrained RNN, please check /Models/ to verify if the model does exist.")
+            else:
+                print("[SUCCESS] The pretrained RNN model has been loaded successfully.")
+        except FileNotFoundError as e:
+            print("[ERROR] The following error occured while trying to load the model: "+str(e))
+
+    def extract_character(self, image: bytes) -> dict:
+        """
+        Extract character is a RNN method that should accept bytes image data and then makes prediction for it. 
+        """
+        try:
+            self.base_image_data = image
+            self.image = self.read_grayscale_image(image_data= self.base_image_data)
+            self.image = self._preprocess_image(self.image)
+            predictions = self.rnn.predict(self.image)
+            predicted_class = np.argmax(predictions[0])
+
+            return {
+                "Result": "Success",
+                "Model": "RNN",
+                "Type": "Sequential",
+                "Prediction": predicted_class,
+                "Confidences": [str(x) for x in predictions[0]],
+                "Total Confidences": len(predictions[0])
+            }
+        except Exception as e:
+            return {"Result": "Failed",
+                    "ERROR": str(e),
+                    "ERR at": "hindi_ocr.RNN.extract_character"}
+        
+    def _preprocess_image(self, img: np.ndarray)-> np.ndarray:
+        """
+        Predict_with_Preprocessing is an private method that preprocesses image for classification
+        """
+        try:
+
+            img_array = image.img_to_array(img)
+            img_array_reshaped = np.expand_dims(img_array, axis=0)
+            img_array_normalized = img_array_reshaped / 255.0
+            
+            
+            return img_array_normalized
+        except Exception as e:
+            return {"Result": "Failed",
+                    "ERROR": str(e),
+                    "ERR at": "hindi_ocr.CNN.(PRIVATE_METHOD)"}
 
 def test_cnn():
     obj = CNN()
@@ -118,3 +191,13 @@ def test_cnn():
         encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
 
     print(obj.extract_character(encoded_image))
+
+def test_rnn():
+    obj = RNN()
+    path = "ManualTests/La.png"
+    with open(path, "rb") as image_file:
+        encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+
+    print(obj.extract_character(encoded_image))
+
+test_rnn()
